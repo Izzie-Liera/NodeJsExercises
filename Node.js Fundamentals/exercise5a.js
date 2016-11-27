1842eb36994d2a2bdd1fca0fe30f774af18b8068
Isabel Liera Leal <liera90@hotmail.com>
2016-11-26 21:34:10 -0600

    Exporting functions

---
+++ Node.js Fundamentals/exercise5a.js
@@ -0,0 +1,17 @@
+// Exercise #5a
+// This exercise will execute "exercise5b.js" file, using require.
+
+console.log('Starting...');
+
+const fs = require('fs');
+const os = require('os');
+//var user = os.userInfo();
+
+// const that stores the location where the other file will be required:
+const notes = require('./exercise5b.js');
+
+//
+var res = notes.addNote();
+console.log(res);
+
+console.log('Completed!');
---
+++ Node.js Fundamentals/exercise5b.js
@@ -0,0 +1,10 @@
+// Exercise #5b
+// This exercise will execute
+
+console.log(module);
+
+module.exports.addNote = () =>
+{
+  console.log('Function called...');
+  return "Message returned: New note";
+};
